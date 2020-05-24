082a9134dc3765740ddcf817e9a38bece2e06338
JillMOry <oryjill@gmail.com>
2020-05-24 17:56:26 -0500

    todo_app day_1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isClicked: false,
+			todos: [],
+			text: ""
+		};
+	}
+
+	handleClick = () => {
+		this.setState({
+			isClicked: true,
+			todos: [...this.state.todos, this.state.text],
+			text: ""
+		});
+	};
+
+	handleDelete = (index) => () => {
+		const todosCopy = [...this.state.todos];
+		todosCopy.splice(index, 1);
+		this.setState({
+			todos: todosCopy
+		});
+	};
+
+	onChange = (e) => {
+		this.setState({
+			text: e.target.value
+		});
+	};
+
+	render() {
+		return (
+			<header className="App-header">
+				<div className="App">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						<code>Welcome To The Sweet Life</code>
+					</p>
+
+					<button onClick={this.handleClick}>
+						{this.state.isClicked ? "TRUE!!" : "FALSE!!!"}
+					</button>
+
+					<input value={this.state.text} onChange={this.onChange} />
+
+					{this.state.todos.map((todo, index) => (
+						<div key={index}>
+							<button onClick={this.handleDelete(index)}>delete</button>
+							{todo}
+						</div>
+					))}
+				</div>
+			</header>
+		);
+	}
 }
 
 export default App;
